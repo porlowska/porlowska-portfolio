71d6f750e63e41d194cf93693cfb54e4b1c339ff
Polly <147508946+porlowska@users.noreply.github.com>
2023-12-27 17:04:46 +0000

    added projects layout

--- assets/text-editor.js
+++ assets/text-editor.js
@@ -1,9 +1,28 @@
-const header = $('h1');
-const headerText =$('#pHeader');
-const about = $('#hAbout');
-const aboutText = $('#pAbout');
+// Header section
+$('h1').text('Front-End Web Developer');
+$('#pHeader').text("I'm thrilled to share my journey with you. Let's make things happen!");
 
-header.text('Front-End Web Developer');
-headerText.text("I'm thrilled to share my journey with you. Let's make things happen!");
-about.text('Hi, I am Polly! Nice to meet you!');
-aboutText.text('Innovative problem solver, passionate about web development and JavaScript, seeking a challenging opportunity in a team where I can grow and develop my skills!'+$('<br>')+'Front-end web developer with a background in business development. Trained at edX University of Colombia coding bootcamp.');
+
+// About Section
+$('#hAbout').text('Hi, I am Polly! Nice to meet you!');
+$('#pAbout').text('Innovative problem solver, passionate about web development and JavaScript, seeking a challenging opportunity in a team where I can grow and develop my skills! Front-end web developer with a background in business development. Trained at edX University of Colombia coding bootcamp.');
+
+// Project section 
+//Slide One
+$('#imgOne') //.attr('src') and alt!
+$('#hOne')
+
+
+//Slide Two
+$('#imgTwo')
+$('#hTwo')
+
+
+//Slide Three
+$('#imgThree')
+$('#hThree')
+
+
+//Animated Description 
+$('card-header')
+$('.card-text')
