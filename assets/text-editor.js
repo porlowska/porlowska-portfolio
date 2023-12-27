1ddefd75796beb3ecfafb918c07acfa5235106aa
Polly <147508946+porlowska@users.noreply.github.com>
2023-12-27 15:52:57 +0000

    added js to overwirte the placeholder text

---
+++ assets/text-editor.js
@@ -0,0 +1,9 @@
+const header = $('h1');
+const headerText =$('#pHeader');
+const about = $('#hAbout');
+const aboutText = $('#pAbout');
+
+header.text('Front-End Web Developer');
+headerText.text("I'm thrilled to share my journey with you. Let's make things happen!");
+about.text('Hi, I am Polly! Nice to meet you!');
+aboutText.text('Innovative problem solver, passionate about web development and JavaScript, seeking a challenging opportunity in a team where I can grow and develop my skills!'+$('<br>')+'Front-end web developer with a background in business development. Trained at edX University of Colombia coding bootcamp.');
